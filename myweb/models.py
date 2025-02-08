d2afeb8dc387144bc30397731c89844f7df7a7dd
= <=>
2025-02-08 21:39:41 +0330

    2025/8/2changes

--- myweb/models.py
+++ myweb/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
 
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateTimeField()
-    amount =models.BigIntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField(default=now)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(default=now)
+    amount = models.BigIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
